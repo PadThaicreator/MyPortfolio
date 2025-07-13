2f7c21393b055c393789e8b60f950644741b049b
PadThaicreator <namozaa62@gmail.com>
2025-07-13 12:02:39 +0700

    Update aboutMeSection.tsx

--- Portfolio_Frontend/src/pages/home/components/aboutMeSection.tsx
+++ Portfolio_Frontend/src/pages/home/components/aboutMeSection.tsx
@@ -1,7 +1,7 @@
 export const AboutMePage = () => {
   return (
     <fieldset className=" card-section border ">
-      <legend className="hover:text-blue-400 hover:font-semibold duration-300 text-2xl font-semibold">About Me</legend>
+      <legend className="  text-2xl font-semibold">About Me</legend>
       <div className="p-3 text-black font-normal">
         Hi! I'm Poonnawit Poosakul, a 4th-year student at Kasetsart University,
         Kamphaeng Saen Campus. I’m passionate about learning new things,
